74204152877656c71ec8841bf9eccc2895eca80b
Thái <hoangthai.txt@gmail.com>
2023-06-13 14:26:01 +0700

    changed api

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -100,7 +100,7 @@ const signin = async (req, res, next) => {
             // httpOnly: true,
             maxAge: 1 * 60 * 60 * 1000,
             // domain: 'localhost',
-            domain: 'finder-api.onrender.com',
+            domain: 'api-41z0.onrender.com',
             // path: '/api/v1',
             // secure: true,
             // sameSite: true
@@ -109,7 +109,7 @@ const signin = async (req, res, next) => {
             // httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
             // domain: 'localhost',
-            domain: 'finder-api.onrender.com',
+            domain: 'api-41z0.onrender.com',
             // path: '/api/v1',
             // secure: true,
             // sameSite: true,
