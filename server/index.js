1a56e0542b745702e026ff9a47c3ab60947b7f59
Thái <hoangthai.txt@gmail.com>
2023-06-14 11:30:22 +0700

    del middleware in index.js

--- server/index.js
+++ server/index.js
@@ -21,24 +21,6 @@ const corsOptions = {
             }
         }
 }
-app.use((req, res, next) => {
-    const origin = req.get('referer')
-    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w))
-    if (isWhitelisted) {
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization')
-        res.setHeader('Access-Control-Allow-Credentials', true)
-    }
-    if (req.method === 'OPTIONS') res.sendStatus(200)
-    else next()
-})
-
-const setContext = (req, res, next) => {
-    if (!req.context) req.context = {}
-    next()
-}
-app.use(setContext)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
