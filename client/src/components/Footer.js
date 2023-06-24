40d044a091a32efb14563ea5f3fdc49cd40d3203
tungdang <tungdang2k0@gmail.com>
2023-06-24 13:56:04 +0700

    fix bug

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,37 +4,36 @@ import logo from '../../src/asset/image/logo.png'
 
 const Footer = () => {
     return (
-        <footer class="bg-[#030014] text-center text-white">
-            <div class="container px-6 pt-6 m-0">
-                <div class="mb-6 flex justify-center">
+        <div className="bg-[#030014]  text-white w-full">
+            <div className="  p-4 text-center">
+                <div className="mb-6 flex justify-center">
                     <img src={logo} alt="logo" />
                 </div>
 
-                <div class="grid md:grid-cols-2 lg:grid-cols-4">
-                    <div class="mb-6">
-                        <span class="mb-2.5  uppercase">Chính sách bảo mật</span>
+                <div className="grid md:grid-cols-2 lg:grid-cols-4">
+                    <div className="mb-6">
+                        <span className="mb-2.5  uppercase">Chính sách bảo mật</span>
                     </div>
-                    <div class="mb-6">
-                        <span class="mb-2.5  uppercase">Giúp đỡ</span>
+                    <div className="mb-6">
+                        <span className="mb-2.5  uppercase">Giúp đỡ</span>
                     </div>
-                    <div class="mb-6">
-                        <span class="mb-2.5  uppercase">Thiết bị hỗ trợ</span>
+                    <div className="mb-6">
+                        <span className="mb-2.5  uppercase">Thiết bị hỗ trợ</span>
                     </div>
-                    <div class="mb-6">
-                        <span class="mb-2.5  uppercase">Về Finder</span>
+                    <div className="mb-6">
+                        <span className="mb-2.5  uppercase">Về Finder</span>
                     </div>
                 </div>
             </div>
-
             <div
-                class="p-4 text-center"
+                className="p-4 text-center"
             >
                 © 2023 Copyright:
-                <a class="text-white" href="https://tailwind-elements.com/"
+                <a className="text-white" href="https://tailwind-elements.com/"
                 >by Finder</a
                 >
             </div>
-        </footer>
+        </div>
     )
 }
 
