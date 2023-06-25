05892049efee763af8f3b283e5db95b5b00d047f
Thái <hoangthai.txt@gmail.com>
2023-06-25 12:38:22 +0700

    config email

--- server/src/routes/media.route.js
+++ server/src/routes/media.route.js
@@ -4,7 +4,6 @@ import mediaController from '../controllers/media.controller.js'
 import requestHandler from '../handlers/request.handler.js'
 import tokenMiddleware from '../middlewares/token.middleware.js'
 import authorizeMiddleware from '../middlewares/authorize.middleware.js'
-import transportController from '../controllers/transport.controller.js'
 
 // MergeParams cho phép truyền các tham số của router cha xuống router con
 const router = express.Router({ mergeParams: true })
@@ -20,9 +19,5 @@ router.post(
     requestHandler.validate,
     mediaController.addGenres,
 )
-
-// Gọi hàm gửi email
-
-router.post('/email', transportController.sendEmail)
 
 export default router
