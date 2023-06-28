47d0d3d165f4456e5e37d5c85a70b1faf6dbb02c
Thái <hoangthai.txt@gmail.com>
2023-06-28 13:55:16 +0700

    create notify for admin, movie, user

--- server/src/controllers/notification.controller.js
+++ server/src/controllers/notification.controller.js
@@ -1,41 +1,128 @@
-import responseHandler from '../handlers/response.handler';
+import responseHandler from '../handlers/response.handler.js'
 import { EventEmitter } from 'events'
+import tokenMiddleware from '../middlewares/token.middleware.js'
+import userModel from '../models/user.model.js'
+import {Server} from 'socket.io'
 
 const eventEmitter = new EventEmitter()
+let io
 
-const notify = async (req, res) => {
-    res.setHeader('Content-Type', 'text/event-stream')
-    res.setHeader('Cache-Control', 'no-cache')
-    res.setHeader('Connection', 'keep-alive')
-    res.setHeader('Access-Control-Allow-Origin', '*')
+// Thiết lập socket.io
+const setupSocketIO = (server) => {
+    io = new Server(server)
 
-    // Gửi thông báo cho người dùng khi có sự kiện
-    const sendNotification = (message) => {
-        res.write(`data: ${JSON.stringify(message)}\n\n`)
-    }
+    // Lắng nghe sự kiện kết nối của người dùng
+    io.on('connection', (socket) => {
+        console.log('Người dùng đã kết nối:', socket.id)
 
-    // Nghe sự kiện từ admin
-    eventEmitter.on('notification', sendNotification)
-
-    // Đóng kết nối khi người dùng ngắt kết nối
-    res.on('close', () => {
-        eventEmitter.off('notification', sendNotification)
+        // Ngắn kết nối khi người dùng disconnect
+        socket.on('disconnect', () => {
+            console.log('Người dùng đã ngắt kết nối:', socket.id)
+        })
     })
 }
 
-const adminPushNotify = (req, res) => {
+const adminPushNotify = async (req, res) => {
     try {
-        const message = { text: 'Thông báo mới từ admin' }
+        const { title, content } = req.body
 
-        // Gửi thông báo tới tất cả người dùng đang kết nối
-        eventEmitter.emit('notification', message)
+        // Lấy danh sách tất cả người dùng
+        const users = await userModel.find({})
+        if (!users) return responseHandler.badrequest(res, 'Không tìm thấy user nào!')
 
-        responseHandler.ok(res, 'Thông báo thành công.')
+        // Gửi thông báo cho mỗi người dùng trong danh sách
+        for (const user of users) {
+            user.notifications.push({ title, content })
+            await user.save()
+        }
+
+        // Gửi thông báo cho tất cả người dùng thông qua socket.io
+        io.emit('notification', { title, content })
+
+        responseHandler.ok(res, {
+            statusCode: 200,
+            message: 'Gửi thông báo thành công!',
+        })
     } catch (error) {
-        console.log(error);
-        responseHandler.error(res, 'Thông báo không thành công.')
+        console.log(error)
+        responseHandler.error(res, 'Gửi thông báo không thành công.')
     }
 }
 
+const sendMovieUpdateNotification = async (title, content) => {
+    try {
+        // Lấy danh sách tất cả người dùng
+        const users = await userModel.find({})
+        if (!users) return responseHandler.badrequest(res, 'Không tìm thấy user nào!')
 
-export default { notify, adminPushNotify }
+        // Gửi thông báo cho mỗi người dùng trong danh sách
+        for (const user of users) {
+            user.notifications.push({ title, content })
+            await user.save()
+        }
+
+        // Gửi thông báo cho tất cả người dùng thông qua socket.io
+        io.emit('notification', { title, content })
+
+        console.log('Gửi thông báo thành công.')
+    } catch (error) {
+        console.log('Gửi thông báo không thành công:', error)
+    }
+}
+
+const sendWelcomeNotification = async (userId) => {
+    try {
+        // Tìm người dùng trong cơ sở dữ liệu bằng userId
+        const user = await userModel.findById(userId)
+
+        // Kiểm tra xem người dùng có tồn tại hay không
+        if (!user) return responseHandler.badrequest(res, 'Người dùng không tồn tại.')
+
+        // Tạo nội dung thông báo chào mừng
+        const title = 'Chào mừng bạn đến với bình nguyên vô tận!'
+        const content = 'Xin chào, ' + user.username + '! Chúc bạn có trải nghiệm tuyệt vời.'
+
+        // Thêm thông báo vào danh sách thông báo của người dùng
+        user.notifications.push({ title, content })
+
+        // Lưu thông tin người dùng đã được cập nhật
+        await user.save()
+
+        // Gửi thông báo chào mừng cho người dùng thông qua socket.io
+        io.to(userId).emit('notification', { title, content })
+
+        console.log('Gửi thông báo chào mừng thành công.')
+    } catch (error) {
+        console.log('Gửi thông báo chào mừng không thành công:', error)
+    }
+}
+
+const deleteWelcomeNotification = async (userId) => {
+    try {
+        // Tìm người dùng trong cơ sở dữ liệu bằng userId
+        const user = await userModel.findById(userId)
+
+        // Kiểm tra xem người dùng có tồn tại hay không
+        if (!user) return responseHandler.badrequest(res, 'Người dùng không tồn tại.')
+
+        // Xoá thông báo chào mừng trong danh sách thông báo của người dùng
+        user.notifications = user.notifications.filter(
+            (notification) => notification.title !== 'Chào mừng bạn đến với bình nguyên vô tận!',
+        )
+
+        // Lưu thông tin người dùng đã được cập nhật
+        await user.save()
+
+        console.log('Xoá thông báo chào mừng thành công.')
+    } catch (error) {
+        console.log('Xoá thông báo chào mừng không thành công:', error)
+    }
+}
+
+export default {
+    setupSocketIO,
+    adminPushNotify,
+    sendMovieUpdateNotification,
+    sendWelcomeNotification,
+    deleteWelcomeNotification,
+}
