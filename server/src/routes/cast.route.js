1d68041b86de23c076e624c89a2a236d9d28efa9
Thái <hoangthai.txt@gmail.com>
2023-06-30 20:37:56 +0700

    add route, validate

--- server/src/routes/cast.route.js
+++ server/src/routes/cast.route.js
@@ -11,6 +11,20 @@ router.get('/', castController.getAllCasts)
 router.get('/:castId', castController.getCastById)
 router.get('/:castId/film', castController.getFilmOfCast)
 
+router.get(
+    '/find-cast/find',
+    tokenMiddleware.auth,
+    castController.findCast
+)
+
+router.put(
+    '/update/:castId',
+    body('name').exists().withMessage('Tên diễn viên không được trống!').isLength({ min: 2 }).withMessage('Tên diễn viên phải lớn hơn 2 ký tự.'),
+    tokenMiddleware.auth,
+    authorizeMiddleware.allowAdminOnly,
+    castController.updateCast
+)
+
 router.post(
     '/',
     body('name')
--- server/src/routes/media.route.js
+++ server/src/routes/media.route.js
@@ -10,14 +10,41 @@ const router = express.Router({ mergeParams: true })
 
 router.get('/media/search', mediaController.search)
 router.get('/', mediaController.getGenres)
+
+router.get(
+    '/find-genre',
+    tokenMiddleware.auth,
+    mediaController.findGenre
+)
+
 router.post(
     '/',
-    body('name').exists().withMessage('name is required').isLength({ min: 1 }).withMessage('name minimum 1 character'),
+    body('name')
+        .exists()
+        .withMessage('Tên thể loại không được trống!')
+        .isLength({ min: 2 })
+        .withMessage('Tên thể loại phải lớn hơn 2 ký tự.'),
     tokenMiddleware.auth,
 
     authorizeMiddleware.allowAdminOnly,
     requestHandler.validate,
     mediaController.addGenres,
+)
+
+router.put(
+    '/update/:genreId',
+    body('name').exists().withMessage('Tên thể loại không được trống!').isLength({min: 2}).withMessage('Tên thể loại phải lớn hơn 2 ký tự.'),
+    tokenMiddleware.auth,
+    authorizeMiddleware.allowAdminOnly,
+    requestHandler.validate,
+    mediaController.updateGenre,
+)
+
+router.delete(
+    '/delete/:genreId',
+    tokenMiddleware.auth,
+    authorizeMiddleware.allowAdminOnly,
+    mediaController.deleteGenre
 )
 
 export default router
