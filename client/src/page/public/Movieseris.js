438b3a6743e1eaee1594c3e902290fdab107de8d
tungdang <tungdang2k0@gmail.com>
2023-07-04 15:05:42 +0700

    no message

--- client/src/page/public/Movieseris.js
+++ client/src/page/public/Movieseris.js
@@ -5,6 +5,7 @@ import SwiperCore, { Navigation, Pagination } from 'swiper'
 import 'swiper/swiper.min.css'
 import 'swiper/swiper-bundle.min.css'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import axios from 'axios'
 
 import { Section, Banner } from '../../components'
 import * as apis from '../../apis'
@@ -13,6 +14,7 @@ SwiperCore.use([Navigation, Pagination])
 const Movieseris = () => {
     const [randomMovies, setRandomMovies] = useState([])
     const [top10Movies, setTop10Movies] = useState(null)
+    const movieId = randomMovies?._id
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,7 +38,14 @@ const Movieseris = () => {
         }
         top10Movies()
     }, [])
-
+    const handleGetApiUPview = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_API_URI}/movies/${movieId}/view`)
+            console.log(response);
+        } catch (error) {
+            console.error(error)
+        }
+    }
     const swiperParams = {
         slidesPerView: 5,
         slidesPerGroup: 1,
@@ -80,7 +89,7 @@ const Movieseris = () => {
     }
     return (
         <div className=" ">
-            <Banner randomMovies={randomMovies} />
+            <Banner randomMovies={randomMovies} handleGetApiUPview={handleGetApiUPview} />
             <div className="relative top-[-10.3125rem] z-[6] bottom-0 left-0 mt-[100px] lg:mt-0">
                 <div className=" my-4">
                     <h3 className="text-white mb-2 px-[48px] text-[18px] font-bold dark:text-main-300 ">Phim truyền hình mới</h3>
